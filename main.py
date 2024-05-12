835fd5410acc8eb1338b729504031d8dc790f334
namssi95 <namssi95@gmail.com>
2024-05-13 02:28:01 +0900

    ✨ 글쓰기 기능 구현
    - 프론트엔드 글쓰기 페이지 form으로 작업했습니다.
    - 백엔드와 SQLite를 연동해서 이미지 및 데이터를 업로드하는 기능을 구현했습니다.

---
+++ main.py
@@ -0,0 +1,64 @@
+from fastapi import FastAPI,UploadFile,Form,Response
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from fastapi.staticfiles import StaticFiles
+from typing import Annotated
+import sqlite3
+
+# 아래는 일단 알아만두기
+con = sqlite3.connect('db.db',check_same_thread=False)
+cur = con.cursor()
+
+app = FastAPI()
+
+@app.post('/items')
+async def create_item(image:UploadFile,
+                title:Annotated[str,Form()],
+                price:Annotated[int,Form()],
+                description:Annotated[str,Form()],
+                place:Annotated[str,Form()],
+                insertAt:Annotated[int,Form()]
+                ):
+#   print(image,title,price,description,place,insertAt)
+
+
+   image_bytes = await image.read()
+   # f""" """ 는 js로지면 ``과 같다
+   cur.execute(f"""
+               INSERT INTO
+               items(title,image,price,description,place,insertAt)
+               VALUES ('{title}','{image_bytes.hex()}',{price},'{description}','{place}',{insertAt})
+               """)
+   # .hex()   : 16진법으로 바꿔 줌
+   con.commit()
+
+@app.get('/items')
+async def get_items():
+   # 컬렴명도 같이 가져옴
+   con.row_factory = sqlite3.Row
+   cur = con.cursor()
+   rows = cur.execute(f"""
+                     SELECT * from items;
+                      """).fetchall()   
+   
+   # return '200'
+   return JSONResponse(jsonable_encoder(dict(row) for row in rows))
+
+@app.get('/images/{item_id}')
+async def get_image(item_id):
+   cur = con.cursor()
+   image_bytes = cur.execute(f"""
+                              SELECT image from items WHERE id={item_id}
+                             """).fetchone()[0] # [0] :그룹껍떼기? 하나 벗기기 위함
+   return Response(content=bytes.fromhex(image_bytes))
+   # bytes: 2진법 , .fromhex() :16진법을 바꾼다. , :~~해서 response하겠다.
+   
+
+# 꼭 가장 아래에 작성하기
+app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
+
+
+# uvicorn main:app --reload   : server 생성
+
+# pip install python-multipart
+# 서버주소/docs   :api 상세내용 확인 가능
